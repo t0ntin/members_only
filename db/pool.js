079d62e289898501c66866c5887dd44fb3e90e87
t0ntin <115113636+t0ntin@users.noreply.github.com>
2025-09-18 17:10:19 -0500

    Edited pool.js.

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,23 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
 
+
+// const pool = new Pool({
+//   host: "localhost", 
+//   user: "achaparro",
+//   database: "club",
+//   password: "",
+//   port: 5432 
+// });
+
+// export default pool;
+import pkg from 'pg';
+const { Pool } = pkg;
 
 const pool = new Pool({
-  host: "localhost", 
-  user: "achaparro",
-  database: "club",
-  password: "ustele",
-  port: 5432 
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 export default pool;
